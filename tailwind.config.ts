03906fb7d96725ce238aa6a65356dc572640cf1a
Junaid Ali Shah Gigli <overview.jjj@gmail.com>
2025-03-09 18:13:28 +0500

    added anlytics and fixes

---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+// tailwind.config.js or tailwind.config.ts
+module.exports = {
+  experimental: {
+    // Disable the Oxide optimizer
+    optimizeUniversalDefaults: false,
+  },
+  // ...other Tailwind configurations
+};
