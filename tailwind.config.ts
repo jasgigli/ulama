379e06c5fede36d0acb97ea2f9098d79f6d874f3
Junaid Ali Shah Gigli <overview.jjj@gmail.com>
2025-03-09 18:57:39 +0500

    Fix: Reinstall node_modules and update lockfile

--- tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-// tailwind.config.js or tailwind.config.ts
-module.exports = {
-  experimental: {
-    // Disable the Oxide optimizer
-    optimizeUniversalDefaults: false,
-  },
-  // ...other Tailwind configurations
-};
